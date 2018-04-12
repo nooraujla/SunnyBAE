ae67e5dc29ab51ec62d914974b9aa1470acd69df
Noorpreet Aujla <Noorpreet.Aujla@capitalone.com>
2018-04-12 11:12:06 -0700

    Initial Commit

---
+++ SunIsBAE/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SunIsBAE
+//
+//  Created by Aujla, Noorpreet on 4/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
